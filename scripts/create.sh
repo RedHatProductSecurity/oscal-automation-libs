2c2e66ca660fa39dd67c046e130a7eb19792e616
Jennifer Power <barnabei.jennifer@gmail.com>
2023-06-12 15:18:10 -0400

    feat: adds automation to manage ssp index during generation
    Signed-off-by: Jennifer Power <barnabei.jennifer@gmail.com>

--- scripts/create.sh
+++ scripts/create.sh
@@ -7,12 +7,10 @@ SCRIPT_DIR="$(realpath "$(dirname "$BASH_SOURCE")")"
 source "$SCRIPT_DIR/logging.sh"
 # shellcheck disable=SC1091
 source "$SCRIPT_DIR/trestle.sh"
+# shellcheck disable=SC1091
+source "$SCRIPT_DIR/manage_ssps.sh"
 
 function generate_ssp() {
-    # Function requires the following variables
-    vars=("comps" "output" "profile")
-    var_check "${vars[@]}"
-
     # shellcheck disable=SC2154
     run_log 0 "Generating Markdown for ${output}"
     # shellcheck disable=SC2154
@@ -21,23 +19,8 @@ function generate_ssp() {
 
 
 function assemble_ssp() {
-    # Function requires the following variables
-    vars=("comps" "output")
-    var_check "${vars[@]}"
-    
     run_log 0 "Assembling to JSON for ${output}"
     trestle author ssp-assemble --name "$output" --compdefs "$comps" --markdown markdown/system-security-plans/"$output" --output "$output"
-}
-
-
-function create_ssp() {
-    # Function requires the following variables
-    vars=("comps" "output" "profile")
-    var_check "${vars[@]}"
-
-    run_log 0 "Creating new SSP ${output}"
-    generate_ssp
-    assemble_ssp
 }
 
 function var_check() {
@@ -47,4 +30,15 @@ function var_check() {
         run_log 1 "Required variable $i not set"
       fi
     done
+}
+
+function create_ssp() {
+    # Function requires the following variables
+    vars=("comps" "output" "profile")
+    var_check "${vars[@]}"
+
+    run_log 0 "Creating new SSP ${output}"
+    generate_ssp
+    assemble_ssp
+    update_ssp_index "$output" "$comps"
 }
---
+++ scripts/manage_ssps.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+# shellcheck disable=SC2128
+SCRIPT_DIR="$(realpath "$(dirname "$BASH_SOURCE")")"
+
+# shellcheck disable=SC1091
+source "$SCRIPT_DIR/logging.sh"
+
+# Specify the default dictionary file
+DEFAULT_SSP_INDEX="ssp_index.txt"
+
+function new_index() {
+    local file=${1:?file is required}
+    header="##### This file is managed by automation. DO NOT EDIT. #####"
+
+    # Write the header to the file (overwrite existing content)
+    echo "$header" > "$file"
+}
+
+# Function to update the ssp index with with new associated
+# component definitions. This allows for automated
+# regeneratation and assembly of a particular set of
+# ssps with compliance-trestle
+function update_ssp_index() {
+    local ssp="${1:?ssp is required}"
+    local comps="${2:?comps is required}"
+    local index_file="${3:-${DEFAULT_SSP_INDEX}}"
+
+    # Declare the associative array
+    declare -A index
+
+    # Create the index file if it doesn't exist
+    if [ ! -f "$index_file" ]; then
+        new_index $index_file
+    else 
+        # Read the index file into the associative array
+        while IFS=":" read -r key value; do
+            index["$key"]=$value
+        done < "$index_file"
+    fi
+
+    # Specify the key and value to check and update
+    key=$ssp
+    new_value=$comps
+
+    # Check if the key exists and update the value if needed
+    if [ "${index[$key]+exists}" ]; then
+    if [ "${index[$key]}" != "$new_value" ]; then
+        index["$key"]=$new_value
+        rm "$index_file"
+        new_index $index_file
+        for key in "${!index[@]}"; do
+            echo "$key:${index[$key]}" >> "$index_file"
+        done
+        run_log 0 "Value updated for key '$key'"
+    else
+        run_log 0 "Value for key '$key' is already up to date"
+    fi
+    else
+    index["$key"]=$new_value
+    rm "$index_file"
+    new_index $index_file
+    for key in "${!index[@]}"; do
+        echo "$key:${index[$key]}" >> "$index_file"
+    done
+    run_log 0 "Key '$key' added to the index"
+    fi
+}
+
+
